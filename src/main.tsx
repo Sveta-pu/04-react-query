7fdd28497dda6c447bea6311f5fa402d24b1f5c4
Sveta <pupsadon@gmail.com>
2025-12-14 19:10:45 +0200

    import 'index.css'

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import 'modern-normalize';
+import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
