8c412b64935655712bc8e2a7aadae308769635ea
Sveta <pupsadon@gmail.com>
2025-12-14 19:13:42 +0200

    modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import './index.css';
+import 'modern-normalize/modern-normalize.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
