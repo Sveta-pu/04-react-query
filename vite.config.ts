bf3e42660d972377e795bfce545730b8d8f49108
Sveta <pupsadon@gmail.com>
2025-12-14 21:32:29 +0200

    Fix vite base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/04-react-query/',
+  base: process.env.GITHUB_ACTIONS ? '/04-react-query/' : '/',
   plugins: [react()],
 });
